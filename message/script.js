4edde0aed5612f9e2acd023daf438e98822429b7
CHOSSEANG <jng00153@gmail.com>
2025-08-17 12:03:43 +0900

    메세지 > message-content-header 부분 구현

--- message/script.js
+++ message/script.js
@@ -1,4 +1,6 @@
 const msgListEl = document.querySelector('#message-list');
+const msgContentHeaderEl = document.querySelector('#message-content-header');
+let userArray = [];
 
 function createMessageList(num){
     
@@ -19,16 +21,25 @@ function createMessageList(num){
         // 메세지 div에 텍스트 요소 추가
         textEl.append(textUserEl);
         textEl.append(textContentEl);
+        userArray.push({name:"User_"+i,content:"Content_"+i});
         textUserEl.innerText='User_' + i;
         textContentEl.innerText='Content_' + i;
         textEl.classList.add('message-box-text');
         messageEl.append(textEl);
         messageEl.classList.add('message-box');
+        messageEl.setAttribute('data-id',''+i);
+        messageEl.addEventListener('click',(event)=>{
+            const dataId = messageEl.getAttribute('data-id');
+            if(msgContentHeaderEl.childElementCount >0){
+                msgContentHeaderEl.lastElementChild.remove();
+            }
+            document.getElementById('message-content-header').style.border = '1px solid lightgray';
+            msgContentHeaderEl.append(messageEl.cloneNode("deep"));
+        })
 
         // 메세지 list에 메세지 div 추가
         msgListEl.append(messageEl);
     }
-
 }
 
-createMessageList(2);
+createMessageList(3);
